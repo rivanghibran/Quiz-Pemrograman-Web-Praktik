140a7b520e590d6130b8829a6376037221d2c1c1
RivanGhibran <rivanghibran26@gmail.com>
2024-10-30 10:01:00 +0700

    update

---
+++ config/db_config.py
@@ -0,0 +1,12 @@
+from flask_mysqldb import MySQL
+from flask import Flask
+
+def init_db(app: Flask):
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_DB'] = 'responsi'
+
+    # Inisialisasi MySQL
+    mysql = MySQL(app)
+    return mysql
--- myapp.py
+++ myapp.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
+from config.db_config import init_db
 
 app = Flask(__name__)
 app.secret_key = 'pw'
+mysql = init_db(app)
 
 @app.route('/')
 def home():
     return render_template('homepage.html', title='Home Page', name='World' )
 
 @app.route('/about')
-def register():
-    return render_template('about.html', title='About Us' )
+def about():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM students") 
+        data = cur.fetchall()
+        cur.close()
+        return render_template('about.html', title='About Us', students=data ) 
+    except Exception as e:
+        return f"An error occurred: {e}"
+    
 
 
 if __name__ == '__main__':
